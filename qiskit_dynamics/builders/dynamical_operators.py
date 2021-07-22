65c1a6f688fddd987a606eba2a223b0949e0ea22
Haggai Landa <haggai.landa@ibm.com>
2021-07-22 15:26:02 +0300

    Simulation building skeleton code

--- qiskit_dynamics/builders/dynamical_operators.py
+++ qiskit_dynamics/builders/dynamical_operators.py
@@ -6,51 +6,70 @@ from qiskit.quantum_info.operators.base_operator import BaseOperator
 
 
 class DynamicalOperator(ABC):
+	"""An class for operators used in defining dynamical simulations."""
 
-	DEFAULT_ALIASES =\
-	{
-		# Note that both strings must be lower case
-		'id': 'i',
-		'sx': 'x',
-		'sy': 'y',
-		'sz': 'z',
-	}
+	DEFAULT_ALIASES = \
+		{
+			# Note that both strings must be lower case
+			'id': 'i',
+			'sx': 'x',
+			'sy': 'y',
+			'sz': 'z',
+		}
+	"""Aliases available by default for the two-level qubit operators that are realized in this class."""
 
-	def __init__(self, s_id: Any = '', s_type = '', aliases = None):
-		self.s_id = s_id
+	def __init__(self, system_id: Any = '', s_type = '', aliases = None):
+		""" Initialization of an operator using (optional) id of the subsystem, type, and aliases.
+
+		Args:
+			system_id: A unique identifier of the subsystem (degree of freedom) of the operator,
+				or an empty string to defer such an identification.
+			s_type: A string name of the type of operator.
+			aliases: A dictionary of aliases for operator types, allowing users to expand the
+				s_type names used for known operators. If ``None``, the field ``DEFAULT_ALIASES``
+				is used.
+		"""
+		self.system_id = system_id
 		self._s_type = ''
 		self._s_type_unique = ''
 		if aliases is None:
 			aliases = self.DEFAULT_ALIASES
-		self.aliases = aliases  # must be assigned before setting self.s_type
-		self.s_type = s_type
+		self.aliases = aliases
+		# must be assigned first, before setting the property self.s_type, which searches the aliases.
+
+		self.s_type = s_type  # must be assigned after self.aliases, because it searches the aliases.
 		self.compound_type = ''
 		self.compound_ops = None
 
 	def __deepcopy__(self, memo = None):
-		cc = DynamicalOperator(self.s_id, self.s_type, self.aliases)
+		cc = DynamicalOperator(self.system_id, self.s_type, self.aliases)
 		cc.compound_type = self.compound_type
 		cc.compound_ops = deepcopy(self.compound_ops, memo)
 		return cc
 
 	def new_operator(self):
+		"""A method that must be implemented by subclasses, to return the correct instance."""
 		return DynamicalOperator(aliases = self.aliases)
 
 	@property
 	def s_type(self) -> str:
+		"""A string defining the operator type. Aliases (multiple names) may be used."""
 		return self._s_type
 
 	@property
 	def s_type_unique(self) -> str:
+		"""A unique string defining the operator type, which all aliases must map to."""
 		return self._s_type_unique
 
 	@s_type.setter
 	def s_type(self, s_type):
+		"""A setter of the operator's type, which searches aliases and sets the unique type as well."""
 		self._s_type = s_type
 		s_type = self.s_type.lower()  # should be here after assigning _s_type with user's string as is
 		self._s_type_unique = self.aliases.get(s_type, s_type)
 
 	def __add__(self, other):
+		"""Addition of two DynamicalOperators. Returns a new (compound) DynamicalOperator."""
 		if not isinstance(other, DynamicalOperator):
 			raise Exception("Both operands in an addition must be instances of a DynamicalOperator.")
 		result = self.new_operator()
@@ -67,9 +86,11 @@ class DynamicalOperator(ABC):
 	# 	return result
 
 	def __sub__(self, other):
+		"""Subtraction of two DynamicalOperators. Returns a new (compound) DynamicalOperator."""
 		return self.__add__(-other)
 
 	def __mul__(self, other):
+		"""Multiplication by a DynamicalOperator or a scalar."""
 		result = self.new_operator()
 		if isinstance(other, DynamicalOperator):
 			result.compound_type = '@'
@@ -84,17 +105,28 @@ class DynamicalOperator(ABC):
 		raise Exception("The second operand of a multiplication must be a DynamicalOperator class or a scalar.")
 
 	def __rmul__(self, other):
+		"""Multiplication of a DynamicalOperator by a scalar."""
 		result = self.__mul__(other)
 		return result
 
 	def __neg__(self):
+		"""Unary negation of a DynamicalOperator."""
 		result = self.__rmul__(-1.)
 		return result
 
 	def __pos__(self):
+		"""Unary plus operator prepending a DynamicalOperator."""
 		return self
 
 	def get_operator_matrix(self, s_type_unique, dim: int) -> np.ndarray:
+		"""Returns a matrix describing a concrete realization of the DynamicalOperator.
+
+		This function is not declared as static in order to allow subclasses to override the
+		implementation. However, fields of the ``self`` object are not being used.
+		Args:
+			s_type_unique: A unique operator type name to generate.
+			dim: The physical dimension of the matrix to generate.
+		"""
 		# TODO: replace qubit creation with Qiskit operator.from_label()
 		# TODO: support 'i x y z sp sm n p q a a_ n n^2 0 1 p_0_1'
 		if dim == 2:
@@ -115,17 +147,19 @@ class DynamicalOperator(ABC):
 			elif s_type_unique == '1':
 				return np.asarray([[0, 0], [0, 1]], complex)
 		# Just a partial implementation for now
-		raise Exception(f"Operator type {self.s_type} unknown or unsupported for matrix generation with dimension {dim}.")
+		raise Exception(
+			f"Operator type {s_type_unique} unknown or unsupported for matrix generation with dimension {dim}.")
 
 
 class DynamicalOperatorKey:
+	"""A placeholder for a unique key identifying an operator and a subsystem."""
 
 	def __init__(self, op: DynamicalOperator):
-		self.s_id = op.s_id
+		self.system_id = op.system_id
 		self.s_type_unique = op.s_type_unique
 
 	def __hash__(self):
-		return hash((self.s_id, self.s_type_unique))
+		return hash((self.system_id, self.s_type_unique))
 
 
 class OperatorBuilder(ABC):
@@ -232,14 +266,15 @@ class OperatorBuilder(ABC):
 			sub_matrices = {}
 			for key_element in key:
 				operator_key: DynamicalOperatorKey = key_element
-				dim = self._subsystem_dims.get(operator_key.s_id, None)
+				dim = self._subsystem_dims.get(operator_key.system_id, None)
 				if dim is None:
-					raise Exception(f"An operator was defined with id = {operator_key.s_id}, but this id does not appear in the subsystem_dims parameter.")
+					raise Exception(
+						f"An operator was defined with id = {operator_key.system_id}, but this id does not appear in the subsystem_dims parameter.")
 				new_sub_matrix = self._dyn_op.get_operator_matrix(operator_key.s_type_unique, dim)
-				sub_matrix = sub_matrices.get(operator_key.s_id, None)
+				sub_matrix = sub_matrices.get(operator_key.system_id, None)
 				if sub_matrix is not None:
 					new_sub_matrix = sub_matrix @ new_sub_matrix  # note that order of matrix product matters
-				sub_matrices[operator_key.s_id] = new_sub_matrix
+				sub_matrices[operator_key.system_id] = new_sub_matrix
 			op_matrix = None
 			n_subsystems = len(self._ids)
 			for i in range(n_subsystems):
@@ -253,31 +288,30 @@ class OperatorBuilder(ABC):
 
 
 class Sx(DynamicalOperator):
-	def __init__(self, s_id = ''):
-		super().__init__(s_id, 'x')
+	def __init__(self, system_id = ''):
+		super().__init__(system_id, 'x')
 
 
 class Sy(DynamicalOperator):
-	def __init__(self, s_id = ''):
-		super().__init__(s_id, 'y')
+	def __init__(self, system_id = ''):
+		super().__init__(system_id, 'y')
 
 
 class Sz(DynamicalOperator):
-	def __init__(self, s_id = ''):
-		super().__init__(s_id, 'z')
+	def __init__(self, system_id = ''):
+		super().__init__(system_id, 'z')
 
 
 class Sp(DynamicalOperator):
-	def __init__(self, s_id = ''):
-		super().__init__(s_id, 'sp')
+	def __init__(self, system_id = ''):
+		super().__init__(system_id, 'sp')
 
 
 class Sm(DynamicalOperator):
-	def __init__(self, s_id = ''):
-		super().__init__(s_id, 'sm')
+	def __init__(self, system_id = ''):
+		super().__init__(system_id, 'sm')
 
 
 class Sid(DynamicalOperator):
-	def __init__(self, s_id = ''):
-		super().__init__(s_id, 'id')
-
+	def __init__(self, system_id = ''):
+		super().__init__(system_id, 'id')
