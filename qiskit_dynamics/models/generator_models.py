450fde19f89972e909bfd920407f0d4fe24dc6e5
Avery Parr <averyparr@gmail.com>
2021-07-26 12:49:46 -0400

    Import cleanup

--- qiskit_dynamics/models/generator_models.py
+++ qiskit_dynamics/models/generator_models.py
@@ -25,7 +25,7 @@ from qiskit.quantum_info.operators import Operator
 from qiskit_dynamics.models.operator_collections import DenseOperatorCollection
 from qiskit_dynamics import dispatch
 from qiskit_dynamics.dispatch import Array
-from qiskit_dynamics.signals import Signal, SignalList,SignalSum
+from qiskit_dynamics.signals import Signal, SignalList
 from .frame import BaseFrame, Frame
 
 
