40a73b9681bcb20198ff74948dff041bb53010bd
Avery Parr <averyparr@gmail.com>
2021-07-29 11:09:04 -0400

    Bugfix with frames in Lindblad Model.

--- qiskit_dynamics/models/lindblad_models.py
+++ qiskit_dynamics/models/lindblad_models.py
@@ -55,9 +55,9 @@ class LindbladModel(GeneratorModel):
 
     def __init__(
         self,
-        hamiltonian_operators: List[Operator],
+        hamiltonian_operators: Array,
         hamiltonian_signals: Union[List[Signal], SignalList],
-        dissipator_operators: Optional[List[Operator]] = None,
+        dissipator_operators: Array = None,
         dissipator_signals: Optional[Union[List[Signal], SignalList]] = None,
         drift: Optional[Array] = None,
         frame: Optional[Union[Operator, Array, Frame]] = None,
@@ -113,7 +113,7 @@ class LindbladModel(GeneratorModel):
     def from_hamiltonian(
         cls,
         hamiltonian: HamiltonianModel,
-        dissipator_operators: Optional[List[Operator]] = None,
+        dissipator_operators: Optional[Array] = None,
         dissipator_signals: Optional[Union[List[Signal], SignalList]] = None,
     ):
         """Construct from a :class:`HamiltonianModel`.
@@ -142,9 +142,7 @@ class LindbladModel(GeneratorModel):
     @frame.setter
     def frame(self, frame: Union[Operator, Array, Frame]):
         if self._frame is not None and self._frame.frame_diag is not None:
-            self._operator_collection.drift = self._operator_collection.drift + Array(
-                np.diag(self._frame.frame_diag)
-            )
+            self._operator_collection.drift = self._operator_collection.drift + self._frame.operator_into_frame_basis(frame)
             self._operator_collection.apply_function_to_operators(
                 self.frame.operator_out_of_frame_basis
             )
@@ -155,9 +153,7 @@ class LindbladModel(GeneratorModel):
             self._operator_collection.apply_function_to_operators(
                 self.frame.operator_into_frame_basis
             )
-            self._operator_collection.drift = self._operator_collection.drift - Array(
-                np.diag(self._frame.frame_diag)
-            )
+            self._operator_collection.drift = self._operator_collection.drift - self._frame.operator_into_frame_basis(frame)
 
     def evaluate_without_state(self, time: float, in_frame_basis: Optional[bool] = False) -> Array:
         raise NotImplementedError("Lindblad models cannot be represented without a given state without vectorization.")
@@ -181,14 +177,16 @@ class LindbladModel(GeneratorModel):
         # Need to check that I have the differences chosen correctly
         if self.frame.frame_diag is not None:
             frame_eigvals = self.frame.frame_diag
-            pexp = np.exp((1j * time) * frame_eigvals)
-            nexp = np.exp((-1j * time) * frame_eigvals)
-            # Hopefully equivalent to rhs = e^{iFt} \rho e^{-iFt}
+            pexp = np.exp(-time * frame_eigvals) # e^{iHt} = e^{-tF}
+            nexp = np.exp(time * frame_eigvals) #e^{-iHt} = e^{tF}
+            # Equivalent to rhs = e^{iHt} \rho e^{-iHt}
             rhs = np.outer(pexp, nexp) * y
+
             rhs = self._operator_collection.evaluate_with_state(
                 [hamiltonian_sig_vals, dissipator_sig_vals], rhs
             )
-            # Hopefully equivalent to rhs = e^{-iFt} rhs e^{iFt}
+
+            # Equivalent to rhs = e^{-iHt} rhs e^{iHt}
             rhs = np.outer(nexp, pexp) * rhs
         else:
             rhs = self._operator_collection.evaluate_with_state(
