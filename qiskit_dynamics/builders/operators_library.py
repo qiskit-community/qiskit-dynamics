bcf6b215f5149f894c61a32ade68c6c4503beacb
Haggai Landa <haggai.landa@ibm.com>
2021-08-11 12:34:26 +0300

    Added more operators

--- qiskit_dynamics/builders/operators_library.py
+++ qiskit_dynamics/builders/operators_library.py
@@ -23,12 +23,36 @@ Currently implemented are spin operators and truncated harmonic oscillator opera
 in addition to some more general operators, all realizing numpy matrices by default.
 The classes ``Id`` and ``Zero`` are implemented separately, in the file dynamical_operators.py
 together with the base class ``DynamicalOperator``.
+Currently supported:
+Identity, Zero, and a standard basis Projector in any dimension.
+Spin-1/2 (qubit) operators: x, y, z, sigma^(+/-) and six projectors on the state with +/- eigenvalues
+	along the axes.
+Oscillator canonical operators, ladder operators, number and number^2 operators, in any dimension.
 """
 
-# TODO: Support the following operators at arbitrary dimensions:
-# 		i zero, projectors?
-# 		x y z sp sm 0 1 + - r l, for spins
-# 		n p q a a_ n n^2, for oscillators and
+# TODO: Generalize the spin operators up to a low dimension (say 4 or 5).
+
+
+class Projector(DynamicalOperator):
+	"""A dynamical operator that builds a numpy projector matrix in the standard basis."""
+	def __init__(self, system_id = '', row = 0, col = 0):
+		self._row = row
+		self._col = col
+		super().__init__(system_id, 'proj' + str(row) + '_' + str(col))
+
+	def get_operator_matrix(self, dim: int) -> Any:
+		"""Returns a matrix describing a realization of the operator specified in the parameters.
+
+		Args:
+			dim: The physical dimension of the matrix to generate.
+		"""
+		result = np.zeros((dim, dim), complex)
+		row = self._row; col = self._col
+		if row < dim and col < dim:
+			result[row, col] = 1
+			return result
+		raise Exception(f"A projector with row = {row} and column = {col } "
+			f"is incompatible with matrix generation of dimension {dim}.")
 
 
 class Sx(DynamicalOperator):
@@ -210,6 +234,110 @@ class MinusY(DynamicalOperator):
 		"""
 		if dim == 2 and self.s_type == 'l':
 			return np.asarray([[0.5, 0.5j], [-0.5j, 0.5]], complex)
+		super().get_operator_matrix(dim)
+
+
+class On(DynamicalOperator):
+	"""A dynamical operator that builds a numpy density matrix for an oscillator number operator."""
+
+	def __init__(self, system_id = ''):
+		super().__init__(system_id, 'n')
+
+	def get_operator_matrix(self, dim: int) -> Any:
+		"""Returns a matrix describing a realization of the operator specified in the parameters.
+
+		Args:
+			dim: The physical dimension of the matrix to generate.
+		"""
+		if self.s_type == 'n':
+			return np.diag(np.asarray(range(dim), complex))
+		super().get_operator_matrix(dim)
+
+
+class On2(DynamicalOperator):
+	"""A dynamical operator that builds a numpy density matrix for an oscillator n^2 operator."""
+
+	def __init__(self, system_id = ''):
+		super().__init__(system_id, 'n2')
+
+	def get_operator_matrix(self, dim: int) -> Any:
+		"""Returns a matrix describing a realization of the operator specified in the parameters.
+
+		Args:
+			dim: The physical dimension of the matrix to generate.
+		"""
+		if self.s_type == 'n2':
+			return np.diag(np.asarray(range(dim), complex) ** 2)
+		super().get_operator_matrix(dim)
+
+
+class Oa(DynamicalOperator):
+	"""A dynamical operator that builds a numpy density matrix for an oscillator number operator."""
+
+	def __init__(self, system_id = ''):
+		super().__init__(system_id, 'a')
+
+	def get_operator_matrix(self, dim: int) -> Any:
+		"""Returns a matrix describing a realization of the operator specified in the parameters.
+
+		Args:
+			dim: The physical dimension of the matrix to generate.
+		"""
+		if self.s_type == 'a':
+			return np.diag(np.asarray(range(1, dim), complex) ** .5, 1)
+		super().get_operator_matrix(dim)
+
+
+class Oa_(DynamicalOperator):
+	"""A dynamical operator that builds a numpy density matrix for an oscillator number operator."""
+
+	def __init__(self, system_id = ''):
+		super().__init__(system_id, 'a')
+
+	def get_operator_matrix(self, dim: int) -> Any:
+		"""Returns a matrix describing a realization of the operator specified in the parameters.
+
+		Args:
+			dim: The physical dimension of the matrix to generate.
+		"""
+		if self.s_type == 'a':
+			return np.diag(np.asarray(range(1, dim), complex) ** .5, -1)
+		super().get_operator_matrix(dim)
+
+
+class Oq(DynamicalOperator):
+	"""A dynamical operator that builds a numpy density matrix for an oscillator number operator."""
+
+	def __init__(self, system_id = ''):
+		super().__init__(system_id, 'q')
+
+	def get_operator_matrix(self, dim: int) -> Any:
+		"""Returns a matrix describing a realization of the operator specified in the parameters.
+
+		Args:
+			dim: The physical dimension of the matrix to generate.
+		"""
+		if self.s_type == 'q':
+			return (.5 ** .5) * (np.diag(np.asarray(range(1, dim), complex) ** .5, 1)
+					+ np.diag(np.asarray(range(1, dim), complex) ** .5, -1))
+		super().get_operator_matrix(dim)
+
+
+class Op(DynamicalOperator):
+	"""A dynamical operator that builds a numpy density matrix for an oscillator number operator."""
+
+	def __init__(self, system_id = ''):
+		super().__init__(system_id, 'p')
+
+	def get_operator_matrix(self, dim: int) -> Any:
+		"""Returns a matrix describing a realization of the operator specified in the parameters.
+
+		Args:
+			dim: The physical dimension of the matrix to generate.
+		"""
+		if self.s_type == 'p':
+			return -1j * (.5 ** .5) * (np.diag(np.asarray(range(1, dim), complex) ** .5, 1)
+					- np.diag(np.asarray(range(1, dim), complex) ** .5, -1))
 		super().get_operator_matrix(dim)
 
 
