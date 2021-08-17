21e7f57e2c80cc2ce0a62bb90ed52d6ca53d6a4a
Avery Parr <averyparr@gmail.com>
2021-08-17 14:24:39 -0400

    perform_rotating_wave -> rotating_wave

--- qiskit_dynamics/models/__init__.py
+++ qiskit_dynamics/models/__init__.py
@@ -70,4 +70,4 @@ from .operator_collections import (
     DenseVectorizedLindbladCollection,
     SparseLindbladCollection,
 )
-from .rotating_wave import perform_rotating_wave_approximation
+from .rotating_wave import rotating_wave_approximation
