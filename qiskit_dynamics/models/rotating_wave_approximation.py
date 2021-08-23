22a033319f87b4960fe5e40581958540f0b5d102
Avery Parr <averyparr@gmail.com>
2021-08-23 16:15:07 -0400

    Update docstring

--- qiskit_dynamics/models/rotating_wave_approximation.py
+++ qiskit_dynamics/models/rotating_wave_approximation.py
@@ -9,7 +9,7 @@
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
-# pylint: disable=invalid-name
+# pylint: disable=invalid-name, inconsistent-return-statements
 
 """Functions for performing the Rotating Wave Approximation
 on Model classes."""
@@ -35,17 +35,12 @@ def rotating_wave_approximation(
     return_signal_translator: Optional[bool] = False,
 ) -> BaseGeneratorModel:
     r"""Performs the RWA on Model classes and returns it as a new model.
-    Checks every element :math:`(H_i)_{jk}` of each operator component
-    :math:`H_i`, setting it to zero in the new model if the effective
-    frequency of that element is above some ``cutoff_freq``. Effective
-    frequencies are the sum of the signal frequency :math:`\nu_i`
-    associated to a signal :math:`s_i(t)` as
-    :math:`s_i(t)=Re[a_i(t)e^{2\pi i\nu_i+\phi_i}]` and those due to the
-    rotating frame, of the form :math:`f_{jk}=Im[-d_j+d_k]/2\pi`.
+    Performs elementwise RWA on each operator component with maximal
+    frequency ``cutoff_freq``.
 
     Optionally returns a function ``f`` that translates SignalLists
     defined for the old Model to ones compatible with the new Model, as
-    ``new_model.signals = f(old_model_signal_list)``.
+    ``model.signals = sig_list`` <-> ``rwa_model.signals = f(sig_list)``.
 
     Args:
         model: The GeneratorModel to which you
@@ -62,41 +57,23 @@ def rotating_wave_approximation(
         to pure Signal objects or if a ``CallableGenerator`` is passed.
         ValueError: If there aren't the same number of signals as operators.
 
-    Formalism: When we consider e^{-tF}A e^{tF} in the basis in which F
-    is diagonal, we may write conjugation by e^{\pm tF} as elementwise
-    multiplication s.t. (e^{-tF} A e^{tF})_{jk} = e^{(-d_j+d_k)t}*A_{jk}.
-    When we write e^{-tF}(G(t)-F)e^{tF} = \sum_i e^{-tF} s_i(t)G_ie^{tF}
-    + e^{-tF}(G_d-F)e^{tF} in order to take the RWA in the rotating frame,
-    we must consider the effect of rotations caused by the rotating frame, with
-    frequency Im(-d_j-d_k)/(2*pi), as well as those due to the signal's
-    rotation itself, where we write s_i(t) = Re[a_i(t)e^{i(2*pi*nu_i*t+phi_i)}]
-    = a_i(t)e^{i(2*pi*nu_i*t-phi_i)}/2 + \bar{a_i(t)}e^{-i(2*pi*nu_i*t+phi_i)}/2. With this
-    in mind, consider a term of the form (e^{-tF}s_i(t)G_ie^{tF})_{jk}
-    = e^{(-d_j+d_k)t}(G_i)_{jk}[a_i(t)e^{i(2*pi*nu_i*t+phi_i)}
-    +\bar{a_i(t)}e^{-i(2*pi*nu_i*t+phi_i)}]. The \pm i(...) term has
-    effective frequency \pm nu_i + Im(-d_j+d_k)/(2*pi) = \pm nu_i + f_{jk},
-    (we neglect any oscillation/rotation in a_i for now) and we wish to neglect
-    all terms with effective frequency greater in magnitude than some cutoff
-    frequency nu_*. As such, let us write G_i = A_i + B_i + C_i + D_i, with
-    A_i the matrix representing terms/elements which have
-    1) abs(nu_i+f_{jk})<nu_* and 2) abs(-nu_i+f_{jk})<nu_*, B_i the matrix
-    of terms where 1) but not 2), C_i the matrix of terms where 2) but not 1),
-    and D_i the terms where neither 1) nor 2) hold. Thus, after the RWA,
-    we may write our term as s_i(t) G_i -> a_i(t)e^{i(2*pi*nu_i*t+phi_i)}(A_i+B_i)/2
-    + \bar{a_i(t)}e^{-i(2*pi*nu_i*t+phi_i)}(A_i+C_i)/2 = s_i(t) A_i +
-    [Re[a_i]cos(2*pi*nu_i*t+phi_i)-Im[a_i]sin(2*pi*nu_i*t+phi_i)]B_i/2 +
-    [Im[a_i]cos(2*pi*nu_i*t+phi_i)+Re[a_i]sin(2*pi*nu_i*t+phi_i)]iB_i/2 +
-    [Re[a_i]cos(2*pi*nu_i*t+phi_i)-Im[a_i]sin(2*pi*nu_i*t+phi_i)]C_i/2 +
-    [Im[a_i]cos(2*pi*nu_i*t+phi_i)-Re[a_i]sin(2*pi*nu_i*t+phi_i)](-iC_i/2)
-    = s_i(t)A_i + Re[a_i(t)e^{i(2*pi*nu_i*t+phi_i)}](B_i+C_i)/2 +
-    Re[a_i(t)e^{i(2*pi*nu_i*t+phi_i-pi/2)}](iB_i-iC_i)/2 =
-    s_i(t)(A_i + B_i/2 + C_i/2) + s'_i(t)(iB_i-iC_i)/2 where s'_i
-    is a Signal with frequency nu_i, amplitude a_i, and phase phi_i - pi/2.
+    Formalism: When considering s_i(t) e^{-tF}G_ie^{tF}, in the frame in which
+    F is diagonal, the (jk) element of the i^{th} matrix has effective frequency
+    \tilde\nu_{ijk}^\pm = \pm\nu_i + Im[-d_j+d_k]/2\pi, where the \pm\nu_i comes from
+    expressing s_i(t) = Re[a_i(t)e^{2\pi i\nu_i t}] = a_i(t)e^{i(2\pi\nu_i t+\phi_i)}/2 + c.c.
+    and the other term comes from the rotating frame. Define G_i^\pm the matrix whose
+    entries (G_i^\pm)_{jk} are the entries of G_i s.t. |\nu_{ijk}^\pm|<\nu_* for some
+    cutoff frequency \nu_*. Then, after the RWA, we may write
+    .. math::
+        s_i(t)G_i \to G_i^+ a_ie^{i(2\pi \nu_i t+\phi_i)}/2
+                        + G_i^- \bar{a_i}e^{-i(2\pi \nu_i t+\phi_i)}/2.
 
-    Next, note that the drift terms (G_d)_{jk} have effective frequency
-    Im[-d_j+d_k]/(2*pi). Note that this vanishes on the diagonal,
-    so the -F term may be ignored for any nonzero cutoff_freq.
+    When we regroup these to use only the real components of Signal objects, we find that
+    .. math::
+        s_i(t)G_i \to  s_i(t)(G_i^+ + G_i^-)/2 + s_i'(t)(iG_i^+-iG_i^-)
 
+    where s_i'(t) is a Signal with the same frequency and amplitude as s_i, but with a phase
+    shift of \phi_i - \pi/2. Note that the frame shifts -F are not affected by the RWA.
     """
 
     if isinstance(model, CallableGenerator):
@@ -227,11 +204,7 @@ def get_new_operators(
             in the rotating frame.
         cutoff_freq: maximum frequency allowed under the RWA.
     Returns:
-        Tuple[SignalList,Array]: Tuple of Signal objects (post RWA)
-        and (2k,n,n) Array of new operators post RWA.
-    Raises:
-        NotImplementedError: if components s_j(t) are not equivalent
-        to pure Signal objects.
+        SignaLList: (2k,n,n) Array of new operators post RWA.
     """
 
     num_components = len(current_sigs)
