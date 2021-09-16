aa18babe5d3b2ebc4c84a22f487ddcf6e59f0c67
DanPuzzuoli <dan.puzzuoli@gmail.com>
2021-09-16 11:00:19 -0400

    deleted redundant text

--- qiskit_dynamics/solver_classes.py
+++ qiskit_dynamics/solver_classes.py
@@ -182,24 +182,6 @@ class Solver:
         formatted to be the same types as the input. See Additional Information
         for special handling of various input types.
 
-        Special handling for various input types:
-            - If `y0` is an `Array`, it is passed directly to
-            :func:`~qiskit_dynamics.solve_lmde` as is. Acceptable array shapes are
-            determined by the model type and evaluation mode.
-            - If `y0` is a subclass of :class:`qiskit.quantum_info.QuantumState`:
-                - If `self.model` is a :class:`~qiskit_dynamics.models.LindbladModel`,
-                `y0` is converted to a :class:`DensityMatrix`. Further, if the model
-                evaluation mode is vectorized `y0` will be suitably reshaped for solving.
-                - If `self.model` is a :class:`~qiskit_dynamics.models.HamiltonianModel`,
-                and `y0` a :class:`DensityMatrix`, the full unitary will be simulated,
-                and the evolution of `y0` is attained via conjugation.
-            - If `y0` is a subclass of :class`qiskit.quantum_info.QuantumChannel`, the full
-            evolution map will be computed and composed with `y0`; either the unitary if
-            `self.model` is a :class:`~qiskit_dynamics.models.HamiltonianModel`, or the full
-            Lindbladian `SuperOp` if the model is a :class:`~qiskit_dynamics.models.LindbladModel`.
-
-
-
         Args:
             t_span: Time interval to integrate over.
             y0: Initial state.
