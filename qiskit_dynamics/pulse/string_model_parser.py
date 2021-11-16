10e7cf61b76a33bad31216b98cdc1e72ceeeb688
DanPuzzuoli <dan.puzzuoli@gmail.com>
2021-11-15 16:20:34 -0800

    added operator merging and channel sorting step, and added tests for some string models

--- qiskit_dynamics/pulse/string_model_parser.py
+++ qiskit_dynamics/pulse/string_model_parser.py
@@ -24,8 +24,10 @@ from qiskit_dynamics.dispatch import Array
 
 from .string_model_parser_old.string_model_parser import HamiltonianParser
 
-def parse_hamiltonian_dict(hamiltonian_dict: dict,
-                           subsystem_list: Optional[List[int]] = None) -> Tuple[Array, Array, List[str]]:
+
+def parse_hamiltonian_dict(
+    hamiltonian_dict: dict, subsystem_list: Optional[List[int]] = None
+) -> Tuple[Array, Array, List[str]]:
     """Convert Hamiltonian string representation into concrete operators
     and an ordered list of channels corresponding to the operators.
 
@@ -47,37 +49,29 @@ def parse_hamiltonian_dict(hamiltonian_dict: dict,
     #####################################################################################################
     hamiltonian_pre_parse_exceptions(hamiltonian_dict)
 
-
     ###########################################################
     # construct intermediate representation, where operators are
     # constructed but coefficients are still in string format
     ###########################################################
 
-
     # get variables
     variables = OrderedDict()
-    if 'vars' in hamiltonian_dict:
-        variables = OrderedDict(hamiltonian_dict['vars'])
+    if "vars" in hamiltonian_dict:
+        variables = OrderedDict(hamiltonian_dict["vars"])
 
     # Get qubit subspace dimensions
-    if 'qub' in hamiltonian_dict:
+    if "qub" in hamiltonian_dict:
         if subsystem_list is None:
-            subsystem_list = [int(qubit) for qubit in hamiltonian_dict['qub']]
+            subsystem_list = [int(qubit) for qubit in hamiltonian_dict["qub"]]
         else:
             # if user supplied, make a copy and sort it
             subsystem_list = subsystem_list.copy()
             subsystem_list.sort()
 
         # force keys in hamiltonian['qub'] to be ints
-        qub_dict = {
-            int(key): val
-            for key, val in hamiltonian_dict['qub'].items()
-        }
+        qub_dict = {int(key): val for key, val in hamiltonian_dict["qub"].items()}
 
-        subsystem_dims = {
-            int(qubit): qub_dict[int(qubit)]
-            for qubit in subsystem_list
-        }
+        subsystem_dims = {int(qubit): qub_dict[int(qubit)] for qubit in subsystem_list}
     else:
         subsystem_dims = {}
 
@@ -86,21 +80,17 @@ def parse_hamiltonian_dict(hamiltonian_dict: dict,
     # We don't support this, should we drop it? Need to see to what extent
     # this is required by the HamiltonianParser object
     ##################################################################################################
-    if 'osc' in hamiltonian_dict:
-        oscillator_dims = {
-            int(key): val
-            for key, val in hamiltonian_dict['osc'].items()
-        }
+    if "osc" in hamiltonian_dict:
+        oscillator_dims = {int(key): val for key, val in hamiltonian_dict["osc"].items()}
     else:
         oscillator_dims = {}
 
     # Parse the Hamiltonian
-    system = HamiltonianParser(h_str=hamiltonian_dict['h_str'],
-                               dim_osc=oscillator_dims,
-                               dim_qub=subsystem_dims)
+    system = HamiltonianParser(
+        h_str=hamiltonian_dict["h_str"], dim_osc=oscillator_dims, dim_qub=subsystem_dims
+    )
     system.parse(subsystem_list)
     system = system.compiled
-
 
     ########################################################################################################
     # Next, extract the channels from the system
@@ -108,29 +98,21 @@ def parse_hamiltonian_dict(hamiltonian_dict: dict,
     # Not sure if we should allow or care about this?
     ########################################################################################################
 
+    # extract which channels are associated with which Hamiltonian terms
+    # This code assumes there is at most one channel appearing in each term, and that it
+    # appears at the end.
     channels = []
     for _, ham_str in system:
-        chan_idx = [
-            i for i, letter in enumerate(ham_str) if letter in ['D', 'U']
-        ]
-        for ch in chan_idx:
-            if (ch + 1) == len(ham_str) or not ham_str[ch + 1].isdigit():
-                raise Exception('Channel name must include' +
-                                'an integer labeling the qubit.')
-        for kk in chan_idx:
-            done = False
-            offset = 0
-            while not done:
-                offset += 1
-                if not ham_str[kk + offset].isdigit():
-                    done = True
-                # In case we hit the end of the string
-                elif (kk + offset + 1) == len(ham_str):
-                    done = True
-                    offset += 1
-            temp_chan = ham_str[kk:kk + offset]
-            if temp_chan not in channels:
-                channels.append(temp_chan)
+        chan_idx = None
+        if 'U' in ham_str:
+            chan_idx = ham_str.index('U')
+        elif 'D' in ham_str:
+            chan_idx = ham_str.index('D')
+
+        if chan_idx is None:
+            channels.append(None)
+        else:
+            channels.append(ham_str[chan_idx:])
 
     ####################################################################################################
     # Try to evaluate the coefficients
@@ -142,17 +124,41 @@ def parse_hamiltonian_dict(hamiltonian_dict: dict,
     # this seems like it works
     # set up variables to use in exec computation: set channels to 1 and
     # include variables
-    local_vars = {chan: 1. for chan in channels}
+    local_vars = {chan: 1.0 for chan in set(channels) if chan is not None}
     local_vars.update(variables)
 
-    new_ops = []
+    evaluated_ops = []
     for op, coeff in system:
         loc = {}
-        exec('evaluated_coeff = %s' % coeff, globals(), local_vars)
-        new_ops.append(local_vars['evaluated_coeff'] * op)
+        exec("evaluated_coeff = %s" % coeff, globals(), local_vars)
+        evaluated_ops.append(local_vars["evaluated_coeff"] * op)
 
+    ###################################################################################################
+    # Merge terms based on channel
+    ###################################################################################################
 
-    return system, variables, subsystem_dims, channels, new_ops
+    static_hamiltonian = None
+    hamiltonian_operators = []
+    reduced_channels = []
+
+    for channel, op in zip(channels, evaluated_ops):
+        # if None, add it to the static hamiltonian
+        if channel is None:
+            if static_hamiltonian is None:
+                static_hamiltonian = op
+            else:
+                static_hamiltonian += op
+        else:
+            if channel in reduced_channels:
+                hamiltonian_operators[reduced_channels.index(channel)] += op
+            else:
+                hamiltonian_operators.append(op)
+                reduced_channels.append(channel)
+
+    # sort channels/operators according to channel ordering
+    reduced_channels, hamiltonian_operators = zip(*sorted(zip(reduced_channels, hamiltonian_operators)))
+
+    return static_hamiltonian, list(hamiltonian_operators), list(reduced_channels)
 
 
 def hamiltonian_pre_parse_exceptions(hamiltonian_dict: dict):
@@ -166,28 +172,38 @@ def hamiltonian_pre_parse_exceptions(hamiltonian_dict: dict):
         QiskitError: If some part of the Hamiltonian dictionary is unsupported or invalid.
     """
 
-    ham_str = hamiltonian_dict.get('h_str', [])
-    if ham_str in ([], ['']):
+    ham_str = hamiltonian_dict.get("h_str", [])
+    if ham_str in ([], [""]):
         raise QiskitError("Hamiltonian dict requires a non-empty 'h_str' entry.")
 
-    if hamiltonian_dict.get('qub', {}) == {}:
-        raise QiskitError("Hamiltonian dict requires non-empty 'qub' entry with subsystem dimensions.")
+    if hamiltonian_dict.get("qub", {}) == {}:
+        raise QiskitError(
+            "Hamiltonian dict requires non-empty 'qub' entry with subsystem dimensions."
+        )
 
-    if hamiltonian_dict.get('osc', {}) != {}:
-        raise QiskitError('Oscillator-type systems are not supported.')
+    if hamiltonian_dict.get("osc", {}) != {}:
+        raise QiskitError("Oscillator-type systems are not supported.")
 
     # verify that terms in h_str contain at most a single channel label, and that it
     # is the only thing appearing after ||
-    for term in hamiltonian_dict['h_str']:
-        channel_count = term.count('D') + term.count('U')
+    for term in hamiltonian_dict["h_str"]:
+        channel_count = term.count("D") + term.count("U")
         # verify at most one channel appears
         if channel_count > 1:
-            raise QiskitError("""Hamiltonian term '{}' contains more than one channel label.
-                                 Hamiltonian terms can only contain one channel label.""".format(term))
+            raise QiskitError(
+                """Hamiltonian term '{}' contains more than one channel label.
+                                 Hamiltonian terms can only contain one channel label.""".format(
+                    term
+                )
+            )
 
         # if no channels ensure doesn't contain divider ||
-        elif channel_count == 0 and term.count('|') > 0:
-            raise QiskitError("""Operator-channel divider '||' appears in term '{}' with no channels specified.""".format(term))
+        elif channel_count == 0 and term.count("|") > 0:
+            raise QiskitError(
+                """Operator-channel divider '||' appears in term '{}' with no channels specified.""".format(
+                    term
+                )
+            )
 
         # ensure formatted as aa||Dxx or aa||Uxx where aa specifies an operator and
         # xx is a string of digits
@@ -195,11 +211,17 @@ def hamiltonian_pre_parse_exceptions(hamiltonian_dict: dict):
             malformed_text = """Term '{}' does not conform to required string format.
                                 Channels 'D' or 'U' may only be specified in the format
                                 'aa||Dxx' or 'aa||Uxx', where 'aa' specifies an operator
-                                and 'xx' is a string of digits.""".format(term)
-            if term.count('||') != 1 or term.count('|') > 2:
+                                and 'xx' is a string of digits.""".format(
+                term
+            )
+            if term.count("||") != 1 or term.count("|") > 2:
                 raise QiskitError(malformed_text)
             else:
                 # make sure after || is either U or D followed by a string of digits
-                divider_idx = term.index('||')
-                if term[divider_idx + 2] not in ['D', 'U'] or any([not a.isdigit() for a in term[divider_idx + 3]]):
+                divider_idx = term.index("||")
+                if (
+                    (term[divider_idx + 2] not in ["D", "U"])
+                    or (len(term) == divider_idx + 3)
+                    or any([not a.isdigit() for a in term[divider_idx + 3 :]])
+                ):
                     raise QiskitError(malformed_text)
--- test/dynamics/pulse/test_string_model_parser.py
+++ test/dynamics/pulse/test_string_model_parser.py
@@ -14,13 +14,20 @@
 Tests for pulse.string_model_parser for importing qiskit.pulse model string representation.
 """
 
-from qiskit import QiskitError
+import numpy as np
 
-from qiskit_dynamics.pulse.string_model_parser import hamiltonian_pre_parse_exceptions
+from qiskit import QiskitError
+from qiskit.quantum_info.operators import Operator
+
+from qiskit_dynamics.pulse.string_model_parser import hamiltonian_pre_parse_exceptions, parse_hamiltonian_dict
+from qiskit_dynamics.type_utils import to_array
+
 from ..common import QiskitDynamicsTestCase
+
 
 class TestHamiltonianPreParseExceptions(QiskitDynamicsTestCase):
     """Tests for preparse formatting exceptions."""
+
     ####################################################################################################
     # reconsider where this lives, but for now we need to test
     ###################################################################################################
@@ -36,42 +43,187 @@ class TestHamiltonianPreParseExceptions(QiskitDynamicsTestCase):
         """Test qub dict empty raises error."""
 
         with self.assertRaises(QiskitError) as qe:
-            hamiltonian_pre_parse_exceptions({'h_str': ['a * X0|||D0']})
+            hamiltonian_pre_parse_exceptions({"h_str": ["a * X0|||D0"]})
         self.assertTrue("requires non-empty 'qub'" in str(qe.exception))
 
     def test_too_many_vertical_bars(self):
         """Test that too many vertical bars raises error."""
 
         with self.assertRaises(QiskitError) as qe:
-            hamiltonian_pre_parse_exceptions({'h_str': ['a * X0|||D0'],
-                                              'qub': {0: 2}})
+            hamiltonian_pre_parse_exceptions({"h_str": ["a * X0|||D0"], "qub": {0: 2}})
         self.assertTrue("does not conform" in str(qe.exception))
 
     def test_too_few_vertical_bars(self):
         """Test that too few vertical bars raises error."""
 
         with self.assertRaises(QiskitError) as qe:
-            hamiltonian_pre_parse_exceptions({'h_str': ['a * X0 * D0'],
-                                              'qub': {0: 2}})
+            hamiltonian_pre_parse_exceptions({"h_str": ["a * X0 * D0"], "qub": {0: 2}})
         self.assertTrue("does not conform" in str(qe.exception))
 
     def test_multiple_channel_error(self):
         """Test multiple channels raises error."""
 
         with self.assertRaises(QiskitError) as qe:
-            hamiltonian_pre_parse_exceptions({'h_str': ['a * X0||D0*D1'],
-                                              'qub': {0: 2}})
+            hamiltonian_pre_parse_exceptions({"h_str": ["a * X0||D0*D1"], "qub": {0: 2}})
         self.assertTrue("more than one channel label" in str(qe.exception))
 
     def test_divider_no_channel(self):
         """Test that divider with no channel raises error."""
 
         with self.assertRaises(QiskitError) as qe:
-            hamiltonian_pre_parse_exceptions({'h_str': ['a * X0||'],
-                                              'qub': {0: 2}})
+            hamiltonian_pre_parse_exceptions({"h_str": ["a * X0||"], "qub": {0: 2}})
         self.assertTrue("Operator-channel divider" in str(qe.exception))
 
         with self.assertRaises(QiskitError) as qe:
-            hamiltonian_pre_parse_exceptions({'h_str': ['a * X0|'],
-                                              'qub': {0: 2}})
+            hamiltonian_pre_parse_exceptions({"h_str": ["a * X0|"], "qub": {0: 2}})
         self.assertTrue("Operator-channel divider" in str(qe.exception))
+
+    def test_non_digit_after_channel(self):
+        """Test that everything after the D or U is an int."""
+
+        with self.assertRaises(QiskitError) as qe:
+            hamiltonian_pre_parse_exceptions({"h_str": ["a * X0||Da"], "qub": {0: 2}})
+        self.assertTrue("does not conform" in str(qe.exception))
+
+        with self.assertRaises(QiskitError) as qe:
+            hamiltonian_pre_parse_exceptions({"h_str": ["a * X0||D1a"], "qub": {0: 2}})
+        self.assertTrue("does not conform" in str(qe.exception))
+
+    def test_nothing_after_channel(self):
+        """Test that everything after the D or U is an int."""
+
+        with self.assertRaises(QiskitError) as qe:
+            hamiltonian_pre_parse_exceptions({"h_str": ["a * X0||U"], "qub": {0: 2}})
+        self.assertTrue("does not conform" in str(qe.exception))
+
+
+class TestParseHamiltonianDict(QiskitDynamicsTestCase):
+    """Tests for parse_hamiltonian_dict."""
+
+    def setUp(self):
+        """Build operators."""
+        self.X = Operator.from_label("X").data
+        self.Y = Operator.from_label("Y").data
+        self.Z = Operator.from_label("Z").data
+
+        dim = 4
+        self.a = np.array(np.diag(np.sqrt(range(1, dim)), k=1), dtype=complex)
+        self.adag = self.a.conj().transpose()
+        self.N = np.array(np.diag(range(dim)), dtype=complex)
+
+    def test_simple_single_q_system(self):
+        """Test a basic system."""
+        ham_dict = {
+                    'h_str': ['v*np.pi*Z0', '0.02*np.pi*X0||D0'],
+                    'qub': {
+                        '0': 2
+                    },
+                    'vars': {'v': 2.1}
+                }
+
+        static_ham, ham_ops, channels = parse_hamiltonian_dict(ham_dict)
+
+        self.assertAllClose(static_ham, 2.1 * np.pi * self.Z)
+        self.assertAllClose(to_array(ham_ops), [0.02 * np.pi * self.X])
+        self.assertTrue(channels == ['D0'])
+
+    def test_simple_single_q_system_repeat_entries(self):
+        """Test a basic system."""
+        ham_dict = {
+                    'h_str': ['v*np.pi*Z0', '0.02*np.pi*X0||D0', 'v*np.pi*Z0', '0.02*np.pi*X0||D0'],
+                    'qub': {
+                        '0': 2
+                    },
+                    'vars': {'v': 2.1}
+                }
+
+        static_ham, ham_ops, channels = parse_hamiltonian_dict(ham_dict)
+
+        self.assertAllClose(static_ham, 2 * 2.1 * np.pi * self.Z)
+        self.assertAllClose(to_array(ham_ops), [2 * 0.02 * np.pi * self.X])
+        self.assertTrue(channels == ['D0'])
+
+    def test_simple_two_q_system(self):
+        """Test a two qubit system."""
+
+        ham_dict = {
+                    'h_str': ['v0*np.pi*Z0', 'v1*np.pi*Z1', 'j*np.pi*X0*Y1', '0.03*np.pi*X1||D1', '0.02*np.pi*X0||D0'],
+                    'qub': {
+                        '0': 2,
+                        '1': 2
+                    },
+                    'vars': {'v0': 2.1, 'v1': 2.0, 'j': 0.02}
+                }
+
+        static_ham, ham_ops, channels = parse_hamiltonian_dict(ham_dict)
+
+        ident = np.eye(2)
+
+        self.assertAllClose(static_ham, 2.1 * np.pi * np.kron(ident, self.Z) + 2.0 * np.pi * np.kron(self.Z, ident) + 0.02 * np.pi * np.kron(self.Y, self.X))
+        self.assertAllClose(to_array(ham_ops), [0.02 * np.pi * np.kron(ident, self.X), 0.03 * np.pi * np.kron(self.X, ident)])
+        self.assertTrue(channels == ['D0', 'D1'])
+
+    def test_single_oscillator_system(self):
+        """Test single oscillator system."""
+
+        ham_dict = {
+                    'h_str': ['v*np.pi*O0', 'alpha*np.pi*O0*O0', 'r*np.pi*X0||D0'],
+                    'qub': {
+                        '0': 4
+                    },
+                    'vars': {'v': 2.1, 'alpha': -0.33, 'r': 0.02}
+                }
+
+        static_ham, ham_ops, channels = parse_hamiltonian_dict(ham_dict)
+
+        self.assertAllClose(static_ham, 2.1 * np.pi * self.N - 0.33 * np.pi * self.N * self.N)
+        self.assertAllClose(to_array(ham_ops), [0.02 * np.pi * (self.a + self.adag)])
+        self.assertTrue(channels == ['D0'])
+
+
+    def test_two_oscillator_system(self):
+        """Test a two qubit system."""
+
+        ham_dict = {
+                    'h_str': ['v0*np.pi*O0', 'alpha0*np.pi*O0*O0',
+                              'v1*np.pi*O1', 'alpha1*np.pi*O1*O1',
+                              'j*np.pi*X0*Y1', '0.03*np.pi*X1||D1',
+                              '0.02*np.pi*X0||D0'],
+                    'qub': {
+                        '0': 4,
+                        '1': 4
+                    },
+                    'vars': {'v0': 2.1, 'v1': 2.0, 'alpha0': -0.33, 'alpha1': -0.33, 'j': 0.02}
+                }
+
+        static_ham, ham_ops, channels = parse_hamiltonian_dict(ham_dict)
+
+        ident = np.eye(4)
+
+        self.assertAllClose(static_ham, 2.1 * np.pi * np.kron(ident, self.N)
+                                        - 0.33 * np.pi * np.kron(ident, self.N * self.N)
+                                        + 2.0 * np.pi * np.kron(self.N, ident)
+                                        - 0.33 * np.pi * np.kron(self.N * self.N, ident)
+                                        + 0.02 * np.pi * np.kron(-1j * (self.a - self.adag), self.a + self.adag))
+        self.assertAllClose(to_array(ham_ops), [0.02 * np.pi * np.kron(ident, self.a + self.adag), 0.03 * np.pi * np.kron(self.a + self.adag, ident)])
+        self.assertTrue(channels == ['D0', 'D1'])
+
+
+    def test_single_q_high_dim(self):
+        """Test single q system but higher dim."""
+        ham_dict = {
+                    'h_str': ['v*np.pi*Z0', '0.02*np.pi*X0||D0'],
+                    'qub': {
+                        '0': 4
+                    },
+                    'vars': {'v': 2.1}
+                }
+
+        static_ham, ham_ops, channels = parse_hamiltonian_dict(ham_dict)
+
+        ##############################################################################################
+        # Is this what we want it to be?
+        ##############################################################################################
+        self.assertAllClose(static_ham, 2.1 * np.pi * (np.eye(4) - 2 * self.N))
+        self.assertAllClose(to_array(ham_ops), [0.02 * np.pi * (self.a + self.adag)])
+        self.assertTrue(channels == ['D0'])
