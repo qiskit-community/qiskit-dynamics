71189d05ad4a46975cec9b7269aa69cc5386973c
DanPuzzuoli <dan.puzzuoli@gmail.com>
2022-02-01 13:51:08 -0800

    adding schrieffer_wolff functions

---
+++ qiskit_dynamics/perturbation/schrieffer_wolff.py
@@ -0,0 +1,161 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""
+Schrieffer-Wolff perturbation computation.
+"""
+
+from typing import List, Optional
+
+import numpy as np
+from scipy.linalg import solve_sylvester
+
+from qiskit_dynamics.perturbation import ArrayPolynomial, Multiset
+from qiskit_dynamics.perturbation.multiset import get_all_submultisets
+from qiskit_dynamics.perturbation.solve_lmde_perturbation import merge_expansion_order_indices
+
+
+def schrieffer_wolff(
+    H0: np.ndarray,
+    perturbations: List[np.ndarray],
+    expansion_order: Optional[int] = None,
+    expansion_labels: Optional[List[Multiset]] = None,
+    perturbation_labels: Optional[List[Multiset]] = None,
+    tol: Optional[float] = 1e-15,
+) -> ArrayPolynomial:
+    """Construct truncated multi-variable Schrieffer-Wolff transformation.
+
+    Not sure what the correct output should be but the ``ArrayPolynomial`` will contain
+    all relevant information.
+
+    Also, for now we assume H0 is non-degenerate for initial development, but we
+    should add a "degeneracy_tol" argument for detecting degeneracies when we add this case.
+    More generally we could think about allowing the user to choose the projection,
+    so they could e.g. choose a block-diagonal structure even if there are no degeneracies.
+
+    Further notes to self:
+        - I wrote this initially thinking it could be vectorized, but I think solve_sylvester
+          only works for 2d arrays. Also I just realized that even if solve_sylvester
+          was vectorized I haven't written the solve_commutator_projection function to
+          be vectorized (due to the conditionals). May want to consider modifying things so
+          that it explicitly works with 2d arrays, otherwise the "vectorized" stuff is just
+          confusing to read
+
+    To do:
+        - Maybe validate that that everything is hermitian, and add an anti-hermitian projection
+        step at the end for the perturbation terms
+        - Should we rename H0 to Hd given the usage of 0 as an index?
+    """
+
+    ##################################################################################################
+    # To do: add validation. For validating the expansion_order/labels args we could
+    # move the validation for both solve_lmde_perturbation and this function into
+    # merge_expansion_order_indices (and maybe also move this function into multiset.py)
+    # Also: validate that H0 is diagonal
+    ##################################################################################################
+
+    perturbations = np.array(perturbations)
+    mat_shape = perturbations[0].shape
+
+    if perturbation_labels is None:
+        perturbation_labels = [Multiset({k: 1}) for k in range(len(perturbations))]
+
+    # get all requested terms in the expansion
+    expansion_labels = merge_expansion_order_indices(
+        expansion_order, expansion_labels, perturbation_labels, symmetric=True
+    )
+    expansion_labels = get_all_submultisets(expansion_labels)
+
+    # construct labels for recursive computation
+    recursive_labels = []
+    for label in expansion_labels:
+        for k in range(len(label), 0, -1):
+            recursive_labels.append((label, k))
+
+    # initialize arrays used to store results
+    recursive_A = np.zeros((len(recursive_labels),) + mat_shape, dtype=complex)
+    recursive_B = np.zeros((len(recursive_labels),) + mat_shape, dtype=complex)
+    expansion_terms = np.zeros((len(expansion_labels),) + mat_shape, dtype=complex)
+
+    # right hand side storage
+    rhs_mat = None
+
+    # recursively compute all matrices
+    for (recursive_idx, (expansion_label, commutator_order)) in enumerate(recursive_labels):
+        expansion_idx = expansion_labels.index(expansion_label)
+
+        # initialize rhs matrix at first occurence of current expansion_label
+        if commutator_order == len(expansion_label):
+            rhs_mat = np.zeros(mat_shape, dtype=complex)
+
+        # if commutator_order == 1, all terms required to determine term for current
+        # expansion_label are computed, so solve for the term and initialize
+        # base cases of recursive matrices that depend on it
+        if commutator_order == 1:
+            if expansion_label in perturbation_labels:
+                rhs_mat = rhs_mat + perturbations[expansion_idx]
+                recursive_B[recursive_idx] = perturbations[expansion_idx]
+            # solve for expansion term
+            expansion_terms[expansion_idx] = solve_commutator_projection(H0, rhs_mat, tol=tol)
+
+            # initialize recursive_A base case
+            recursive_A[recursive_idx] = commutator(expansion_terms[expansion_idx], H0)
+        else:
+            # get all 2-fold partitions, bounding the submultisets to have size
+            # <= len(expansion_label) - (commutator_order - 1)
+            submultisets, complements = expansion_label.submultisets_and_complements(
+                len(expansion_label) - commutator_order + 2
+            )
+            for submultiset, complement in zip(submultisets, complements):
+                SI = expansion_terms[expansion_labels.index(submultiset)]
+                recursive_lower_idx = recursive_labels.index((complement, commutator_order - 1))
+                recursive_A[recursive_idx] += commutator(SI, recursive_A[recursive_lower_idx])
+                recursive_B[recursive_idx] += commutator(SI, recursive_B[recursive_lower_idx])
+
+            recursive_A[recursive_idx] = recursive_A[recursive_idx] / commutator_order
+            recursive_B[recursive_idx] = recursive_B[recursive_idx] / (commutator_order - 1)
+            rhs_mat = rhs_mat + recursive_A[recursive_idx] + recursive_B[recursive_idx]
+
+    # project onto anti-hermitian matrices
+    expansion_terms = 0.5 * (expansion_terms - expansion_terms.conj().transpose((0, 2, 1)))
+
+    return ArrayPolynomial(array_coefficients=expansion_terms, monomial_multisets=expansion_labels)
+
+
+def solve_commutator_projection(A: np.ndarray, B: np.ndarray, tol: Optional[float] = 1e-15) -> np.ndarray:
+    """Solve [A, X] = OD(B) assuming A is diagonal, where OD is the projection onto
+    off-diagonal matrices.
+
+    Args:
+        A: 2d array on the LHS of the equation.
+        B: 2d array on the RHS of the equation.
+        tol: Tolerance for determining if OD(B) is 0.
+
+    Returns:
+        Solution to the above problem.
+    """
+
+    # project B onto off diagonal matrices
+    B = B.copy()
+    k = B.shape[-1]
+    B[range(k), range(k)] = 0.0
+
+    # if B is zero after projection, return 0
+    if np.max(np.abs(B)) < tol:
+        return np.zeros(A.shape, dtype=complex)
+
+    return solve_sylvester(A, -A, B)
+
+
+def commutator(A, B):
+    """Matrix commutator."""
+    return A @ B - B @ A
---
+++ test/dynamics/perturbation/test_schrieffer_wolff.py
@@ -0,0 +1,258 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+# pylint: disable=invalid-name
+
+"""Tests for schrieffer_wolff.py."""
+
+import numpy as np
+
+from qiskit_dynamics.perturbation import schrieffer_wolff, Multiset
+from qiskit_dynamics.perturbation.schrieffer_wolff import solve_commutator_projection, commutator
+
+from ..common import QiskitDynamicsTestCase
+
+
+class Testschrieffer_wolff(QiskitDynamicsTestCase):
+    """Test schrieffer_wolff_recursive_construction function."""
+
+    def test_simple_case(self):
+        """Test a simple case with one perturbation using Pauli operators."""
+        X = np.array([[0.0, 1.0], [1.0, 0.0]])
+        Z = np.array([[1.0, 0.0], [0.0, -1.0]])
+
+        H0 = Z
+        H1 = X
+
+        # manually compute SW transformation up to 5th order
+        rhs1 = H1
+        S1 = solve_commutator_projection(H0, rhs1)
+
+        rhs2 = commutator(S1, 0.5 * commutator(S1, H0) + H1)
+        S2 = solve_commutator_projection(H0, rhs2)
+
+        rhs3 = (
+            commutator(S2, 0.5 * commutator(S1, H0) + H1)
+            + commutator(S1, 0.5 * commutator(S2, H0))
+            + commutator(S1, commutator(S1, commutator(S1, H0) / 6 + H1 / 2))
+        )
+        S3 = solve_commutator_projection(H0, rhs3)
+
+        rhs4 = (
+            commutator(S3, 0.5 * commutator(S1, H0) + H1)
+            + commutator(S2, 0.5 * commutator(S2, H0))
+            + commutator(S1, 0.5 * commutator(S3, H0))
+            + commutator(S2, commutator(S1, commutator(S1, H0) / 6 + H1 / 2))
+            + commutator(S1, commutator(S2, commutator(S1, H0) / 6 + H1 / 2))
+            + commutator(S1, commutator(S1, commutator(S2, H0) / 6))
+            + commutator(S1, commutator(S1, commutator(S1, commutator(S1, H0) / 24 + H1 / 6)))
+        )
+        S4 = solve_commutator_projection(H0, rhs4)
+
+        rhs5 = (
+            commutator(S4, 0.5 * commutator(S1, H0) + H1)
+            + commutator(S1, 0.5 * commutator(S4, H0))
+            + commutator(S3, 0.5 * commutator(S2, H0))
+            + commutator(S2, 0.5 * commutator(S3, H0))
+            + commutator(S3, commutator(S1, commutator(S1, H0) / 6 + H1 / 2))
+            + commutator(S1, commutator(S3, commutator(S1, H0) / 6 + H1 / 2))
+            + commutator(S1, commutator(S1, commutator(S3, H0) / 6))
+            + commutator(S2, commutator(S2, commutator(S1, H0) / 6 + H1 / 2))
+            + commutator(S2, commutator(S1, commutator(S2, H0) / 6))
+            + commutator(S1, commutator(S2, commutator(S2, H0) / 6))
+            + commutator(S2, commutator(S1, commutator(S1, commutator(S1, H0) / 24 + H1 / 6)))
+            + commutator(S1, commutator(S2, commutator(S1, commutator(S1, H0) / 24 + H1 / 6)))
+            + commutator(S1, commutator(S1, commutator(S2, commutator(S1, H0) / 24 + H1 / 6)))
+            + commutator(S1, commutator(S1, commutator(S1, commutator(S2, H0) / 24)))
+            + commutator(
+                S1,
+                commutator(
+                    S1, commutator(S1, commutator(S1, commutator(S1, H0) / (24 * 5) + H1 / 24))
+                ),
+            )
+        )
+        S5 = solve_commutator_projection(H0, rhs5)
+
+        expected = np.array([S1, S2, S3, S4, S5])
+        output = schrieffer_wolff(H0, perturbations=[H1], expansion_order=5).array_coefficients
+        self.assertAllClose(expected, output)
+
+    def test_simple_single_variable_polynomial_case(self):
+        """Test a single variable polynomial case."""
+        X = np.array([[0.0, 1.0], [1.0, 0.0]])
+        Y = np.array([[0.0, -1j], [1j, 0.0]])
+        Z = np.array([[1.0, 0.0], [0.0, -1.0]])
+
+        H0 = Z
+        H1 = X
+        H11 = Y
+        H111 = (Y + X + Z) / np.sqrt(3)
+
+        # manually compute SW transformation up to 5th order
+        rhs1 = H1
+        S1 = solve_commutator_projection(H0, rhs1)
+
+        rhs2 = H11 + commutator(S1, 0.5 * commutator(S1, H0) + H1)
+        S2 = solve_commutator_projection(H0, rhs2)
+
+        rhs3 = H111 + (
+            commutator(S2, 0.5 * commutator(S1, H0) + H1)
+            + commutator(S1, 0.5 * commutator(S2, H0) + H11)
+            + commutator(S1, commutator(S1, commutator(S1, H0) / 6 + H1 / 2))
+        )
+        S3 = solve_commutator_projection(H0, rhs3)
+
+        rhs4 = (
+            commutator(S3, 0.5 * commutator(S1, H0) + H1)
+            + commutator(S2, 0.5 * commutator(S2, H0) + H11)
+            + commutator(S1, 0.5 * commutator(S3, H0) + H111)
+            + commutator(S2, commutator(S1, commutator(S1, H0) / 6 + H1 / 2))
+            + commutator(S1, commutator(S2, commutator(S1, H0) / 6 + H1 / 2))
+            + commutator(S1, commutator(S1, commutator(S2, H0) / 6 + H11 / 2))
+            + commutator(S1, commutator(S1, commutator(S1, commutator(S1, H0) / 24 + H1 / 6)))
+        )
+        S4 = solve_commutator_projection(H0, rhs4)
+
+        rhs5 = (
+            commutator(S4, 0.5 * commutator(S1, H0) + H1)
+            + commutator(S1, 0.5 * commutator(S4, H0))
+            + commutator(S3, 0.5 * commutator(S2, H0) + H11)
+            + commutator(S2, 0.5 * commutator(S3, H0) + H111)
+            + commutator(S3, commutator(S1, commutator(S1, H0) / 6 + H1 / 2))
+            + commutator(S1, commutator(S3, commutator(S1, H0) / 6 + H1 / 2))
+            + commutator(S1, commutator(S1, commutator(S3, H0) / 6 + H111 / 2))
+            + commutator(S2, commutator(S2, commutator(S1, H0) / 6 + H1 / 2))
+            + commutator(S2, commutator(S1, commutator(S2, H0) / 6 + H11 / 2))
+            + commutator(S1, commutator(S2, commutator(S2, H0) / 6 + H11 / 2))
+            + commutator(S2, commutator(S1, commutator(S1, commutator(S1, H0) / 24 + H1 / 6)))
+            + commutator(S1, commutator(S2, commutator(S1, commutator(S1, H0) / 24 + H1 / 6)))
+            + commutator(S1, commutator(S1, commutator(S2, commutator(S1, H0) / 24 + H1 / 6)))
+            + commutator(S1, commutator(S1, commutator(S1, commutator(S2, H0) / 24 + H11 / 6)))
+            + commutator(
+                S1,
+                commutator(
+                    S1, commutator(S1, commutator(S1, commutator(S1, H0) / (24 * 5) + H1 / 24))
+                ),
+            )
+        )
+        S5 = solve_commutator_projection(H0, rhs5)
+
+        expected = np.array([S1, S2, S3, S4, S5])
+        output = schrieffer_wolff(
+            H0,
+            perturbations=[H1, H11, H111],
+            perturbation_labels=[Multiset({0: k}) for k in range(1, 4)],
+            expansion_order=5,
+        ).array_coefficients
+        self.assertAllClose(expected, output)
+
+    def test_random_multiple_perturbations(self):
+        """Test with higher dimensional matrices and multiple perturbations."""
+
+        rng = np.random.default_rng(30493)
+        num_terms = 3
+        dim = 5
+        b = 1.0  # bound on size of random terms
+
+        Hd = np.diag(rng.uniform(low=-b, high=b, size=dim))
+
+        # random hermitian ops
+        rand_ops = rng.uniform(low=-b, high=b, size=(4, dim, dim)) + 1j * rng.uniform(
+            low=-b, high=b, size=(4, dim, dim)
+        )
+        rand_ops = 0.5 * (rand_ops + rand_ops.conj().transpose(0, 2, 1))
+
+        H0 = rand_ops[0]
+        H1 = rand_ops[1]
+        H00 = rand_ops[2]
+        H01 = rand_ops[3]
+
+        # manually compute SW transformation up to 5th order
+        rhs0 = H0
+        S0 = solve_commutator_projection(Hd, rhs0)
+
+        rhs1 = H1
+        S1 = solve_commutator_projection(Hd, rhs1)
+
+        rhs00 = H00 + commutator(S0, 0.5 * commutator(S0, Hd) + H0)
+        S00 = solve_commutator_projection(Hd, rhs00)
+
+        rhs01 = (
+            H01
+            + commutator(S0, 0.5 * commutator(S1, Hd) + H1)
+            + commutator(S1, 0.5 * commutator(S0, Hd) + H0)
+        )
+        S01 = solve_commutator_projection(Hd, rhs01)
+
+        rhs11 = commutator(S1, 0.5 * commutator(S1, Hd) + H1)
+        S11 = solve_commutator_projection(Hd, rhs11)
+
+        rhs000 = (
+            commutator(S0, 0.5 * commutator(S00, Hd) + H00)
+            + commutator(S00, 0.5 * commutator(S0, Hd) + H0)
+            + commutator(S0, commutator(S0, commutator(S0, Hd) / 6 + H0 / 2))
+        )
+        S000 = solve_commutator_projection(Hd, rhs000)
+
+        rhs001 = (
+            commutator(S0, 0.5 * commutator(S01, Hd) + H01)
+            + commutator(S1, 0.5 * commutator(S00, Hd) + H00)
+            + commutator(S00, 0.5 * commutator(S1, Hd) + H1)
+            + commutator(S01, 0.5 * commutator(S0, Hd) + H0)
+            + commutator(S0, commutator(S0, commutator(S1, Hd) / 6 + H1 / 2))
+            + commutator(S0, commutator(S1, commutator(S0, Hd) / 6 + H0 / 2))
+            + commutator(S1, commutator(S0, commutator(S0, Hd) / 6 + H0 / 2))
+        )
+        S001 = solve_commutator_projection(Hd, rhs001)
+
+        rhs011 = (
+            commutator(S0, 0.5 * commutator(S11, Hd))
+            + commutator(S1, 0.5 * commutator(S01, Hd) + H01)
+            + commutator(S01, 0.5 * commutator(S1, Hd) + H1)
+            + commutator(S11, 0.5 * commutator(S0, Hd) + H0)
+            + commutator(S0, commutator(S1, commutator(S1, Hd) / 6 + H1 / 2))
+            + commutator(S1, commutator(S0, commutator(S1, Hd) / 6 + H1 / 2))
+            + commutator(S1, commutator(S1, commutator(S0, Hd) / 6 + H0 / 2))
+        )
+        S011 = solve_commutator_projection(Hd, rhs011)
+
+        rhs111 = (
+            commutator(S1, 0.5 * commutator(S11, Hd))
+            + commutator(S11, 0.5 * commutator(S1, Hd) + H1)
+            + commutator(S1, commutator(S1, commutator(S1, Hd) / 6 + H1 / 2))
+        )
+        S111 = solve_commutator_projection(Hd, rhs111)
+
+        rhs0000 = (
+            commutator(S0, 0.5 * commutator(S000, Hd))
+            + commutator(S00, 0.5 * commutator(S00, Hd) + H00)
+            + commutator(S000, 0.5 * commutator(S0, Hd) + H0)
+            + commutator(S0, commutator(S0, commutator(S00, Hd) / 6 + H00 / 2))
+            + commutator(S0, commutator(S00, commutator(S0, Hd) / 6 + H0 / 2))
+            + commutator(S00, commutator(S0, commutator(S0, Hd) / 6 + H0 / 2))
+            + commutator(S0, commutator(S0, commutator(S0, commutator(S0, Hd) / 24 + H0 / 6)))
+        )
+        S0000 = solve_commutator_projection(Hd, rhs0000)
+
+        expected = np.array([S0, S1, S00, S01, S11, S000, S001, S011, S111, S0000])
+        output = schrieffer_wolff(
+            Hd,
+            perturbations=[H0, H1, H00, H01],
+            perturbation_labels=[
+                Multiset({0: 1}),
+                Multiset({1: 1}),
+                Multiset({0: 2}),
+                Multiset({0: 1, 1: 1}),
+            ],
+            expansion_order=3,
+            expansion_labels=[Multiset({0: 4})],
+        ).array_coefficients
+        self.assertAllClose(expected, output)
