a0d26d20df7c5e4155914d24c22964373582c99c
Ben Rosand <benjamin.rosand@ibm.com>
2022-03-28 12:18:17 -0400

    responding to git comments

--- qiskit_dynamics/signals/signals.py
+++ qiskit_dynamics/signals/signals.py
@@ -272,6 +272,10 @@ class DiscreteSignal(Signal):
     The envelope is specified by an array of samples ``s = [s_0, ..., s_k]``, sample width ``dt``,
     and a start time ``t_0``, with the envelope being evaluated as
     :math:`f(t) =` ``s[floor((t - t0)/dt)]``.
+    If the signal is sampled outside of its defined sample list, it will return a 0.0. So if `
+    envelope(t)` is called when `t<start_time` or `t>start_time + (dt * len(samples))`,
+    then `envelope(t)` will return 0.0. This is implemented through a padding method,
+    in which the internal samples are stored with a 0 padded at the end.
     By default a :class:`~qiskit_dynamics.signals.DiscreteSignal` is defined to start at
     :math:`t=0` but a custom start time can be set via the ``start_time`` kwarg.
     """
@@ -296,9 +300,6 @@ class DiscreteSignal(Signal):
             phase: The phase of the carrier. Subclasses such as SignalSums
                    represent the phase of each signal in an array.
             name: name of the signal.
-
-        Raises:
-            QiskitError: if the dimensionality of the samples is greater than 2.
         """
         self._dt = dt
 
@@ -451,7 +452,8 @@ class DiscreteSignal(Signal):
         samples = Array(samples)
 
         if len(samples) < 1:
-            raise QiskitError("Empty sample list")
+            return
+
         if start_sample < len(self.samples):
             raise QiskitError()
 
--- test/dynamics/signals/test_signals.py
+++ test/dynamics/signals/test_signals.py
@@ -902,7 +902,8 @@ class TestSignalsJaxTransformations(QiskitDynamicsTestCase, TestJaxBase):
         )
         self.signal_list = SignalList([self.signal, self.signal_sum, self.discrete_signal])
 
-        # Test jit-compilation of signal evaluation.
+    def test_jit_eval(self):
+        """Test jit-compilation of signal evaluation."""
         self._test_jit_signal_eval(self.signal, t=2.1)
         self._test_jit_signal_eval(self.constant, t=2.1)
         self._test_jit_signal_eval(self.discrete_signal, t=2.1)
