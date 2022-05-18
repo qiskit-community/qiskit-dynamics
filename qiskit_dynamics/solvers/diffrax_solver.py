2ea3c11f61058ac5d70897e4cbb250517e50af36
Ben Rosand <benjamin.rosand@ibm.com>
2022-05-18 12:51:17 -0400

    Revert "linting and diffrax import stuff"
    This reverts commit afc53fa1c6a4f345adbb34f868b0b8f723f088d4.

--- qiskit_dynamics/solvers/diffrax_solver.py
+++ qiskit_dynamics/solvers/diffrax_solver.py
@@ -25,10 +25,7 @@ from qiskit_dynamics.array import Array, wrap
 
 from .solver_utils import merge_t_args, trim_t_results
 
-try:
-    import jax.numpy as jnp
-except ImportError as err:
-    pass
+
 
 
 @requires_backend("jax")
@@ -40,7 +37,14 @@ def diffrax_solver(
     t_eval: Optional[Union[Tuple, List, Array]] = None,
     **kwargs,
 ):
+    try:
+        from diffrax import ODETerm, PIDController, SaveAt
+        from diffrax import diffeqsolve as _diffeqsolve
 
+        from diffrax.solver import AbstractSolver
+        import jax.numpy as jnp
+    except ImportError as err:
+        pass
     """Routine for calling `diffrax.diffeqsolve`
 
     Args:
@@ -54,16 +58,6 @@ def diffrax_solver(
     Returns:
         OdeResult: Results object.
     """
-
-    # We have to import diffrax here so that the imports don't happen when building the docs
-    try:
-        from diffrax import ODETerm, PIDController, SaveAt
-        from diffrax import diffeqsolve as _diffeqsolve
-
-        from diffrax.solver import AbstractSolver
-    except ImportError:
-        pass
-
     if isinstance(method, type) and issubclass(method, AbstractSolver):
         solver = method()
     else:
