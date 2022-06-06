c6e47f6de0797009efedc2925c4f232a897ad8c9
rupeshknn <rupeshknn@gmail.com>
2022-06-07 02:54:26 +0530

    update docstring

--- qiskit_dynamics/solvers/solver_functions.py
+++ qiskit_dynamics/solvers/solver_functions.py
@@ -211,6 +211,7 @@ def solve_lmde(
       ``k_dim`` acts an adjustable accuracy parameter and ``k_dim`` < ``model.dim``. Note
       that the generator must be necessarily hermitian and preferably in sparse evaluation
       mode for better performance.
+    - ``'jax_lanczos_diag'`` JAX implementation of ``'lanczos_diag'``
     - ``'jax_expm'``: JAX-implemented version of ``'scipy_expm'``, with the same arguments and
       behaviour. Note that this method cannot be used for a model in sparse evaluation mode.
     - ``'jax_expm_parallel'``: Same as ``'jax_expm'``, however all loops are implemented using
