048e20a1ea8dd03202842d4faff02750e64a1c5c
rupeshknn <rupeshknn@gmail.com>
2022-06-07 03:58:12 +0530

    fixed imports and np->jnp

--- qiskit_dynamics/solvers/jaxlanczos.py
+++ qiskit_dynamics/solvers/jaxlanczos.py
@@ -15,12 +15,10 @@
 Module containing Lanczos diagonalization algorithm
 """
 
-from typing import Union
-import jax.numpy as np
-from scipy.sparse import csr_matrix
+import jax.numpy as jnp
 
 
-def jax_lanczos_basis(array: np.ndarray, v_0: np.ndarray, k_dim: int):
+def jax_lanczos_basis(array: jnp.ndarray, v_0: jnp.ndarray, k_dim: int):
     """Tridiagonalises a hermitian array in a krylov subspace of dimension k_dim
     using Lanczos algorithm implemented with ``jax``.
 
@@ -34,18 +32,18 @@ def jax_lanczos_basis(array: np.ndarray, v_0: np.ndarray, k_dim: int):
         q_basis : Basis of the krylov subspace.
     """
 
-    data_type = np.result_type(array.dtype, v_0.dtype)
+    data_type = jnp.result_type(array.dtype, v_0.dtype)
     v_0 = v_0.reshape(-1, 1)  # ket
     array_dim = array.shape[0]
-    q_basis = np.zeros((k_dim, array_dim), dtype=data_type)
+    q_basis = jnp.zeros((k_dim, array_dim), dtype=data_type)
 
-    v_p = np.zeros_like(v_0)
-    projection = np.zeros_like(v_0)  # v1
+    v_p = jnp.zeros_like(v_0)
+    projection = jnp.zeros_like(v_0)  # v1
 
-    beta = np.zeros((k_dim,), dtype=data_type)
-    alpha = np.zeros((k_dim,), dtype=data_type)
+    beta = jnp.zeros((k_dim,), dtype=data_type)
+    alpha = jnp.zeros((k_dim,), dtype=data_type)
 
-    v_0 = v_0 / np.sqrt(np.abs(v_0.conj().T @ v_0))
+    v_0 = v_0 / jnp.sqrt(jnp.abs(v_0.conj().T @ v_0))
 
     #     x[idx] = y  -->  x = x.at[idx].set(y)
     q_basis = q_basis.at[[0], :].set(v_0.T)
@@ -60,10 +58,10 @@ def jax_lanczos_basis(array: np.ndarray, v_0: np.ndarray, k_dim: int):
     projection = projection - alpha[0] * v_0
 
     #     x[idx] = y  -->  x = x.at[idx].set(y)
-    beta = beta.at[0].set((np.sqrt(np.abs(projection.conj().T @ projection)))[0, 0])
-    #     beta[0] = np.sqrt(np.abs(projection.conj().T @ projection))
+    beta = beta.at[0].set((jnp.sqrt(jnp.abs(projection.conj().T @ projection)))[0, 0])
+    #     beta[0] = jnp.sqrt(jnp.abs(projection.conj().T @ projection))
 
-    error = np.finfo(np.float64).eps
+    error = jnp.finfo(jnp.float64).eps
 
     for i in range(1, k_dim, 1):
         v_p = q_basis[i - 1, :]
@@ -81,8 +79,8 @@ def jax_lanczos_basis(array: np.ndarray, v_0: np.ndarray, k_dim: int):
         projection = projection - alpha[i] * q_basis[i, :] - beta[i - 1] * v_p
 
         # x[idx] = y  -->  x = x.at[idx].set(y)
-        beta = beta.at[i].set(np.sqrt(np.abs(projection.conj().T @ projection)))
-        # beta[i] = np.sqrt(np.abs(projection.conj().T @ projection))
+        beta = beta.at[i].set(jnp.sqrt(jnp.abs(projection.conj().T @ projection)))
+        # beta[i] = jnp.sqrt(jnp.abs(projection.conj().T @ projection))
 
         # additional steps to increase accuracy
         delta = q_basis[i, :].conj().T @ projection
@@ -97,16 +95,16 @@ def jax_lanczos_basis(array: np.ndarray, v_0: np.ndarray, k_dim: int):
         #     break
 
     tridiagonal = (
-        np.diag(alpha[:k_dim], k=0)
-        + np.diag(beta[: k_dim - 1], k=-1)
-        + np.diag(beta[: k_dim - 1], k=1)
+        jnp.diag(alpha[:k_dim], k=0)
+        + jnp.diag(beta[: k_dim - 1], k=-1)
+        + jnp.diag(beta[: k_dim - 1], k=1)
     )
     q_basis = q_basis[:k_dim]
     q_basis = q_basis.T
     return tridiagonal, q_basis
 
 
-def jax_lanczos_eig(array: np.ndarray, v_0: np.ndarray, k_dim: int):
+def jax_lanczos_eig(array: jnp.ndarray, v_0: jnp.ndarray, k_dim: int):
     """
     Finds the lowest k_dim eigenvalues and corresponding eigenvectors of a hermitian array
     using Lanczos algorithm implemented with ``jax``.
@@ -123,7 +121,7 @@ def jax_lanczos_eig(array: np.ndarray, v_0: np.ndarray, k_dim: int):
     """
 
     tridiagonal, q_basis = jax_lanczos_basis(array, v_0, k_dim)
-    eigen_values, eigen_vectors_t = np.linalg.eigh(tridiagonal)
+    eigen_values, eigen_vectors_t = jnp.linalg.eigh(tridiagonal)
 
     eigen_vectors_a = q_basis @ eigen_vectors_t
 
@@ -131,8 +129,8 @@ def jax_lanczos_eig(array: np.ndarray, v_0: np.ndarray, k_dim: int):
 
 
 def jax_lanczos_expm(
-    array: np.ndarray,
-    v_0: np.ndarray,
+    array: jnp.ndarray,
+    v_0: jnp.ndarray,
     k_dim: int,
     max_dt: float,
 ):
@@ -150,5 +148,5 @@ def jax_lanczos_expm(
     """
 
     q_basis, eigen_values, eigen_vectors_t, _ = jax_lanczos_eig(array, v_0, k_dim)
-    y_dt = q_basis @ eigen_vectors_t @ (np.exp(-1j * max_dt * eigen_values) * eigen_vectors_t[0, :])
+    y_dt = q_basis @ eigen_vectors_t @ (jnp.exp(-1j * max_dt * eigen_values) * eigen_vectors_t[0, :])
     return y_dt
