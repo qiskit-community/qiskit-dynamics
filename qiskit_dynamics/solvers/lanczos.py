c600edfc717319b87493429c46262395232b27e6
rupeshknn <rupeshknn@gmail.com>
2022-06-08 17:41:59 +0530

    lanczos jax for loop --> lax.scan

--- qiskit_dynamics/solvers/lanczos.py
+++ qiskit_dynamics/solvers/lanczos.py
@@ -24,6 +24,7 @@ from qiskit_dynamics.array import Array
 
 try:
     import jax.numpy as jnp
+    from jax.lax import scan
 except ImportError:
     pass
 
@@ -153,74 +154,44 @@ def jax_lanczos_basis(array: Array, v_0: Array, k_dim: int):
         q_basis : Basis of the krylov subspace.
     """
 
-    data_type = jnp.result_type(array.dtype, v_0.dtype)
-    v_0 = v_0.reshape(-1, 1)  # ket
-    array_dim = array.shape[0]
-    q_basis = jnp.zeros((k_dim, array_dim), dtype=data_type)
+    projection_0 = array @ v_0
+    alpha_0 = v_0.conj().T @ projection_0
+    projection_0 = projection_0 - alpha_0 * v_0
+    beta_0 = jnp.sqrt(jnp.abs(projection_0.conj().T @ projection_0))
 
-    v_p = jnp.zeros_like(v_0)
-    projection = jnp.zeros_like(v_0)  # v1
+    initial = jnp.array([*v_0, *projection_0, beta_0])
 
-    beta = jnp.zeros((k_dim,), dtype=data_type)
-    alpha = jnp.zeros((k_dim,), dtype=data_type)
+    def lanczos_iter(carry, dummy):
+        dummy += 0
+        size = (len(carry) - 1) // 2
+        q_p = jnp.array(carry[:size])
+        projection = jnp.array(carry[size : 2 * size])
+        beta_p = carry[2 * size]
 
-    v_0 = v_0 / jnp.sqrt(jnp.abs(v_0.conj().T @ v_0))
+        q_i = projection.T / beta_p
+        projection = array @ q_i
+        alpha_i = q_i.conj().T @ projection
+        projection = projection - alpha_i * q_i - beta_p * q_p
+        beta_i = jnp.sqrt(jnp.abs(projection.conj().T @ projection))
 
-    #     x[idx] = y  -->  x = x.at[idx].set(y)
-    q_basis = q_basis.at[[0], :].set(v_0.T)
-    #     q_basis[[0], :] = v_0.T
+        delta = q_i.conj().T @ projection
+        projection = projection - delta * q_i
+        alpha_i = alpha_i + delta
 
-    projection = array @ v_0
+        carry_next = jnp.array([*q_i, *projection, beta_i])
+        accumulate = jnp.array([alpha_i, beta_i, *q_i])
 
-    #     x[idx] = y  -->  x = x.at[idx].set(y)
-    alpha = alpha.at[0].set((v_0.conj().T @ projection)[0, 0])
-    #     alpha[0] = v_0.conj().T @ projection
+        return carry_next, accumulate
 
-    projection = projection - alpha[0] * v_0
+    _, accumulate_stack = scan(lanczos_iter, initial, None, length=k_dim - 1)
 
-    #     x[idx] = y  -->  x = x.at[idx].set(y)
-    beta = beta.at[0].set((jnp.sqrt(jnp.abs(projection.conj().T @ projection)))[0, 0])
-    #     beta[0] = jnp.sqrt(jnp.abs(projection.conj().T @ projection))
-
-    # error = jnp.finfo(jnp.float64).eps
-
-    for i in range(1, k_dim, 1):
-        v_p = q_basis[i - 1, :]
-
-        # x[idx] = y  -->  x = x.at[idx].set(y)
-        q_basis = q_basis.at[[i], :].set(projection.T / beta[i - 1])
-        # q_basis[[i], :] = projection.T / beta[i - 1]
-
-        projection = array @ q_basis[i, :]  # |array_dim>
-
-        # x[idx] = y  -->  x = x.at[idx].set(y)
-        alpha = alpha.at[i].set(q_basis[i, :].conj().T @ projection)
-        # alpha[i] = q_basis[i, :].conj().T @ projection  # real?
-
-        projection = projection - alpha[i] * q_basis[i, :] - beta[i - 1] * v_p
-
-        # x[idx] = y  -->  x = x.at[idx].set(y)
-        beta = beta.at[i].set(jnp.sqrt(jnp.abs(projection.conj().T @ projection)))
-        # beta[i] = jnp.sqrt(jnp.abs(projection.conj().T @ projection))
-
-        # additional steps to increase accuracy
-        delta = q_basis[i, :].conj().T @ projection
-        projection -= delta * q_basis[i, :]
-
-        # x[idx] = y  -->  x = x.at[idx].set(y)
-        alpha = alpha.at[i].set(alpha[i] + delta)
-        # alpha[i] += delta
-
-        # if beta[i] < error:
-        #     k_dim = i
-        #     break
+    alpha = jnp.array([alpha_0, *accumulate_stack[:, 0]])
+    beta = jnp.array([beta_0, *accumulate_stack[:, 1]])
+    q_basis = jnp.array([v_0, *accumulate_stack[:, 2:]])
 
     tridiagonal = (
-        jnp.diag(alpha[:k_dim], k=0)
-        + jnp.diag(beta[: k_dim - 1], k=-1)
-        + jnp.diag(beta[: k_dim - 1], k=1)
+        jnp.diag(alpha, k=0) + jnp.diag(beta[: k_dim - 1], k=-1) + jnp.diag(beta[: k_dim - 1], k=1)
     )
-    q_basis = q_basis[:k_dim]
     q_basis = q_basis.T
     return tridiagonal, q_basis
 
