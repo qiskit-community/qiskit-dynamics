9c0283143d32ab8d5913ae2cd9c043ac944a7c36
Daniel Puzzuoli <dan.puzzuoli@gmail.com>
2022-12-13 06:34:36 -0800

    Adding jax.Array to the JAX types in dispatch (#162)

--- qiskit_dynamics/dispatch/backends/jax.py
+++ qiskit_dynamics/dispatch/backends/jax.py
@@ -25,6 +25,14 @@ try:
     JAX_TYPES = (DeviceArray, Tracer, JaxprTracer, JVPTracer)
 
     try:
+        # This class was introduced in 0.4.0
+        from jax import Array
+
+        JAX_TYPES += (Array,)
+    except ImportError:
+        pass
+
+    try:
         # This class is not in older versions of Jax
         from jax.interpreters.partial_eval import DynamicJaxprTracer
 
