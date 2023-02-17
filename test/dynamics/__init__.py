86356f29d0346eb35b994a442153ddbf95d24eaa
DanPuzzuoli <dan.puzzuoli@gmail.com>
2023-02-17 11:02:19 -0800

    temporary JAX fix for tests

--- test/dynamics/__init__.py
+++ test/dynamics/__init__.py
@@ -13,3 +13,8 @@
 """
 Qiskit Dynamics tests
 """
+
+# temporarily disable a change in JAX 0.4.4 that introduced a bug. Must be run before importing JAX
+import os
+
+os.environ["JAX_JIT_PJIT_API_MERGE"] = "0"
