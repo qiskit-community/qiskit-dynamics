3bc553526c4587588eb746f183a0df67424077ba
Kento Ueda <38037695+to24toro@users.noreply.github.com>
2023-08-02 20:43:12 +0900

    Update qiskit_dynamics/backend/dynamics_backend.py
    Co-authored-by: Daniel Puzzuoli <dan.puzzuoli@gmail.com>

--- qiskit_dynamics/backend/dynamics_backend.py
+++ qiskit_dynamics/backend/dynamics_backend.py
@@ -609,9 +609,8 @@ class DynamicsBackend(BackendV2):
 
         Args:
             backend: The ``Backend`` instance to build the :class:`.DynamicsBackend` from.
-               Note that even if the `Backend` is :class:`~qiskit.providers.backend.BackendV2`,
-               only :class:`~qiskit.providers.backend.BackendV2` with ``configuration`` and
-               ``defaults`` can be used.
+               Note that while the type hint indicates that `backend` should be a :class:`~qiskit.providers.backend.BackendV1` instance,
+               this method also works for :class:`~qiskit.providers.backend.BackendV2` instances that have been set up with sufficiently populated ``configuration`` and ``defaults`` for backwards compatibility.
             subsystem_list: The list of qubits in the backend to include in the model.
             rotating_frame: Rotating frame argument for the internal :class:`.Solver`. Defaults to
                 ``"auto"``, allowing this method to pick a rotating frame.
