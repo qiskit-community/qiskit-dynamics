5558ed69f635eb6b24fb4c14b13896da72e7c808
Daniel Puzzuoli <dan.puzzuoli@gmail.com>
2024-01-19 08:31:54 -0800

    Updating solvers module to use arraylias (#281)
    Co-authored-by: Kento Ueda <38037695+to24toro@users.noreply.github.com>

--- test/dynamics/solvers/test_jax_odeint.py
+++ test/dynamics/solvers/test_jax_odeint.py
@@ -19,17 +19,18 @@ import numpy as np
 
 from qiskit_dynamics.solvers.jax_odeint import jax_odeint
 
-from ..common import QiskitDynamicsTestCase, TestJaxBase
+from ..common import JAXTestBase
 
 try:
     import jax.numpy as jnp
     from jax.lax import cond
+    from jax import jit, grad
 # pylint: disable=broad-except
 except Exception:
     pass
 
 
-class TestJaxOdeint(QiskitDynamicsTestCase, TestJaxBase):
+class TestJaxOdeint(JAXTestBase):
     """Test cases for jax_odeint."""
 
     def setUp(self):
@@ -129,15 +130,13 @@ class TestJaxOdeint(QiskitDynamicsTestCase, TestJaxBase):
 
         t_span = np.array([0.0, 2.0])
         t_eval = np.array([1.0, 1.5, 1.7])
-        y0 = jnp.array([1.0])
+        y0 = jnp.array([1.0], dtype=complex)
 
         def func(t_s, t_e):
             results = jax_odeint(self.simple_rhs, t_s, y0, t_eval=t_e, atol=1e-10, rtol=1e-10)
-            return results.t.data, results.y.data
+            return results.t, results.y
 
-        jit_func = self.jit_wrap(func)
-
-        t, y = jit_func(t_span, t_eval)
+        t, y = jit(func)(t_span, t_eval)
 
         self.assertAllClose(t_eval, t)
 
@@ -150,7 +149,7 @@ class TestJaxOdeint(QiskitDynamicsTestCase, TestJaxBase):
         )
         self.assertAllClose(expected_y, y)
 
-        jit_grad_func = self.jit_grad_wrap(lambda a: func(t_span, a)[1][-1])
+        jit_grad_func = jit(grad(lambda a: func(t_span, a)[1][-1].real.sum()))
         out = jit_grad_func(t_eval)
         self.assertAllClose(out, np.array([0.0, 0.0, 1.7**2]))
 
@@ -167,7 +166,8 @@ class TestJaxOdeint(QiskitDynamicsTestCase, TestJaxBase):
             return results.y[-1].real.sum()
 
         self.assertAllClose(
-            self.jit_grad_wrap(sim_function)(2.0), 4 * (0.5 + (2.0**3 - 1.0**3) / 3)
+            jit(grad(lambda a: sim_function(a).real.sum()))(2.0),
+            4 * (0.5 + (2.0**3 - 1.0**3) / 3),
         )
 
     def test_empty_integration(self):
