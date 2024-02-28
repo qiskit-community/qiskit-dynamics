8f82c6ebb240f584d6c7e10f60ce139d7e97573e
DanPuzzuoli <dan.puzzuoli@gmail.com>
2024-02-28 08:56:44 -0800

    linting

--- qiskit_dynamics/models/hamiltonian_model.py
+++ qiskit_dynamics/models/hamiltonian_model.py
@@ -82,8 +82,8 @@ class HamiltonianModel(GeneratorModel):
                 F = -iH.
             in_frame_basis: Whether to represent the model in the basis in which the rotating
                 frame operator is diagonalized.
-            array_library: Array library with which to represent the operators in the model, and to 
-                evaluate the model. See the list of supported array libraries in the 
+            array_library: Array library with which to represent the operators in the model, and to
+                evaluate the model. See the list of supported array libraries in the
                 :mod:`.arraylias` submodule API documentation. If ``None``, the arrays will be
                 handled by general dispatching rules.
             validate: If ``True`` check input operators are Hermitian. Note that this is
