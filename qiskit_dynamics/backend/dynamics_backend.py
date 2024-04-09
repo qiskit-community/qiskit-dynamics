3350e8c68220623ba096008b4c1844d36d751f5f
Arthur Strauss <arthurostrauss@gmail.com>
2024-04-09 09:57:53 +0800

    Added release note and docstring

--- qiskit_dynamics/backend/dynamics_backend.py
+++ qiskit_dynamics/backend/dynamics_backend.py
@@ -361,7 +361,8 @@ class DynamicsBackend(BackendV2):
         ``y0`` is not specified, it will be set from ``self.options.initial_state``.
 
         Args:
-            t_span: Time interval to integrate over.
+            t_span: Time interval to integrate over. Defaults to ``None``, in which case the
+                    interval is set to [[0, input.duration] for input in solve_input]].
             y0: Initial state.
             solve_input: Time evolution of the system in terms of quantum circuits or qiskit
                          pulse schedules.
