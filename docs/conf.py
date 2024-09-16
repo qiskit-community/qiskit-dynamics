4609753dba043c17b62443a833512a3f359f34d2
DanPuzzuoli <dan.puzzuoli@gmail.com>
2024-09-16 09:09:56 -0700

    bumping version

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ author = 'Qiskit Development Team'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.5.1'
+release = '0.5.2'
 
 extensions = [
     'sphinx.ext.napoleon',
